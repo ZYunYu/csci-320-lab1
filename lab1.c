fe049970416f78b5689f8df6fdc55cd3b0dcf4a4
Huixiang Zhang <z.yun.yu@outlook.com>
2023-10-06 18:53:17 -0400

    Add allocateString func

--- lab1.c
+++ lab1.c
@@ -1,5 +1,7 @@
-/*
- * Implement your solution in thi file
- *
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include "lab1.h"
 
+char* allocateString() {
+    char* str = (char*)malloc(sizeof(char) * MAX_LINE_LEN);
+}
