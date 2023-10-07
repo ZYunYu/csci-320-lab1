637d95594a4b77052ebe856d7263f89a319005fc
Huixiang Zhang <z.yun.yu@outlook.com>
2023-10-06 21:48:24 -0400

    add readString func

--- lab1.c
+++ lab1.c
@@ -4,4 +4,28 @@
 
 char* allocateString() {
     char* str = (char*)malloc(sizeof(char) * MAX_LINE_LEN);
+    return str;
+}
+
+char* readString(char* fileName) {
+    char* str = allocateString();
+    FILE* file = fopen(fileName, "r");
+    fgets(str, MAX_LINE_LEN, file);
+    fclose(file);
+    return str;
+}
+
+char* mysteryExplode(const char* str) {
+    char* explodedString = allocateString();
+    int i = 0;
+    int j = 0;
+    while (str[i] != '\0') {
+        if (str[i] != ' ') {
+            explodedString[j] = str[i];
+            j++;
+        }
+        i++;
+    }
+    explodedString[j] = '\0';
+    return explodedString;
 }
